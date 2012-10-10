27f17cbf5f299b11f672d0d3cd3bfea2dc8510bf
kurtcc <pritzel@gmail.com>
2012-10-10 15:07:01 +0200

    added isinfo.php

---
+++ Parts/isinfo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!-- Doctype for html5 -->
+<html>
+
+<head></head>
+
+<body>
+
+<?php
+   $cmd = "echo System php function works!";
+
+   $output = system($cmd);
+   // Output of your command echoed to STDOUT
+
+
+
+?>
+</body>
+
+</html>
+
